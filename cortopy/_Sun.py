b8c78c497853ef3c2089601cb438d5bb63ab0332
Michele Maestrini <maestrinimichele@gmail.com>
2024-06-10 17:58:55 +0200

    add headers for sun object

--- cortopy/_Sun.py
+++ cortopy/_Sun.py
@@ -60,18 +60,11 @@ class Sun:
     #@overload
     def __init__(self, name: str, properties: dict) -> None:
         """
-        Description of the method
+        Constructor for the class SUN defining Blender light source
 
         Args:
-            name: name of the CAM object 
-            properties: properties of the CAM object
-
-        Raises:
-            which kind of exceptions
-
-        See also:
-            additional function and modules imported
-
+            name: name of the SUN object 
+            properties: properties of the SUN object
         """
         # SUN name
         self.name = name
@@ -96,23 +89,89 @@ class Sun:
         #def __init__(self, *args, **kwargs) -> None: # version that actually implements the different args checks and implementation
 
     # Instance methods
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Get name of the SUN instance
+
+        Raises:
+            NameError : SUN instance has mismatching name
+
+        Returns:
+            name : name of the SUN object
+        """
         print(self)
         return self.name
 
-    def get_position(self):
+    def get_position(self) -> np.array:
+        """
+        Get position of the SUN instance
+
+        Raises:
+            ValueError : SUN instance has mismatching location
+
+        Returns:
+            position : vector containing the location of the SUN
+        """
+
+        if not all(self.SUN_Blender.location == self.position):
+            raise ValueError("Position mismatch between workspaces.")
+        
         return self.position
     
-    def get_orientation(self):
+    def get_orientation(self) -> np.array:
+        """
+        Get orientation of the SUN instance
+
+        Raises:
+            ValueError : SUN instance has mismatching orientation
+
+        Returns:
+            orientation : vector containing the quaternion representing the orientation of the SUN
+        """
+        if not all(self.SUN_Blender.rotation_quaternion == self.orientation):
+            raise ValueError("orientation mismatch between workspaces.")
+        
         return self.orientation
 
-    def get_energy(self):
+    def get_energy(self) -> float:
+        """
+        Get energy property of the SUN instance
+
+        Raises:
+            ValueError : SUN instance has mismatching fov property
+
+        Returns:
+            energy : scalar containing the fov of the SUN
+        """
+        
+        if self.SUN_Blender.data.energy != self.sun_energy:
+            raise ValueError("property mismatch between workspaces.")
+        
         return self.sun_energy
     
-    def get_angle(self):
+    def get_angle(self) -> float:
+        """
+        Get angle of the SUN instance
+
+        Raises:
+            ValueError : SUN instance has mismatching angle property
+
+        Returns:
+            angle : scalar containing the angle of the SUN instance
+        """
+        
+        if self.SUN_Blender.data.angle != self.sun_angle:
+            raise ValueError("property mismatch between workspaces.")
+        
         return self.sun_angle
         
-    def set_position(self, position:np.array):
+    def set_position(self, position:np.array) -> None:
+        """
+        Set position of the SUN instance
+
+        Args:
+            position : array containing incoming direction of the light source
+        """
         direction = mathutils.Vector(position/np.linalg.norm(position))
         rot_quat = direction.to_track_quat('-Z', 'Y')
         self.position = position
@@ -121,11 +180,23 @@ class Sun:
         self.SUN_Blender.rotation_mode = 'QUATERNION'
         self.SUN_Blender.rotation_quaternion = self.orientation 
 
-    def set_energy(self, energy):
+    def set_energy(self, energy: float) -> None:
+        """
+        Set energy of the SUN instance
+
+        Args:
+            energy : float describing intensity of the light source
+        """
         self.sun_energy = energy
         self.SUN_Blender.data.energy = self.sun_energy
 
-    def set_angle(self, angle):
+    def set_angle(self, angle: float) -> None:
+        """
+        Set angle of the SUN instance
+
+        Args:
+            angle : float describing angle of the light source
+        """
         self.sun_angle = angle
         self.SUN_Blender.data.angle = self.sun_angle
- 
+
