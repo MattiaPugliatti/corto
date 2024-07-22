ccaf237fa1fe92984801e71e3909d829722de93c
Mattia Pugliatti <pugliatti.mattia@gmail.com>
2024-07-22 22:47:09 +0200

    Fix object indexing in json generator

--- tutorials/create_json.py
+++ tutorials/create_json.py
@@ -5,7 +5,12 @@ import numpy as np
 
 data = np.loadtxt('input/S01_Eros/geometry/Cloud_2023_12_06_20_16_48.txt', delimiter=' ').tolist()
 
-GEOM = {"sun" : {"position" : [row[15:19] for row in data]}, "camera" : {"position" :  [row[1:4] for row in data], "orientation" :  [row[4:8] for row in data]}, "body" : {"position" :  [row[8:11] for row in data], "orientation" :  [row[11:15] for row in data]}}
+# [0] ID or ET
+# [1,2,3] Body pos [BU] and [4,5,6,7] orientation [-]
+# [8,9,10] Camera pos [BU] and [11,12,13,14] orientation [-]
+# [15,16,17] Sun pos [BU]
+
+GEOM = {"sun" : {"position" : [row[15:18] for row in data]}, "camera" : {"position" :  [row[8:11] for row in data], "orientation" :  [row[11:15] for row in data]}, "body" : {"position" :  [row[1:4] for row in data], "orientation" :  [row[4:8] for row in data]}}
 # Convert the dictionary to a JSON string
 json_data = json.dumps(GEOM, indent = 4)
 
