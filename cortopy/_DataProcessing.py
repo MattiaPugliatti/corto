7b6b996311903e3800a8d308cd9e96568fafeea5
Mattia Pugliatti <mattia.pugliatti@colorado.edu>
2025-05-08 21:30:44 -0600

    Major changes in implemnetation of the PostProcess pipeline (PART 1 - needs cleaning)

--- cortopy/_DataProcessing.py
+++ cortopy/_DataProcessing.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 from typing import Any, List, Mapping, Optional, Tuple, Union, overload
 
-import bpy
-import os
-import cortopy as corto
-from datetime import datetime
 import warnings
 from glob import glob
 import cv2
+import numpy as np 
+from matplotlib import pyplot as plt
 
 class DataProcessing:
     """
@@ -17,9 +15,165 @@ class DataProcessing:
     # *     Constructors & Destructors
     # *************************************************************************
 
-    def __init__(self) -> None:
+    def __init__(self, img_path: str, label) -> None:
         """
         Constructor for the Data Processing class
+
+        Args:
+            name (str): name of the CAM object
+            properties (dict): properties of the CAM object
+
+        Raises:
+            TypeError : resolution of the camera must be expressed with integer values
         """
 
+        self.img = self.imread(img_path)
+        self.label = label
     # Istance methods
+
+    @staticmethod
+    def imread(path):
+        return cv2.imread(path)
+
+    def crop_image_by_BB(img, BB):
+        x, y, w, h = BB
+        return img[y:y+h, x:x+w]
+    
+    @ staticmethod
+    def resize_image(img,target_res, method:str = 'INTER_NEAREST'):
+        if method == 'INTER_NEAREST':
+            interp_method = cv2.INTER_NEAREST
+        elif method == 'INTER_LINEAR':
+            interp_method = cv2.INTER_LINEAR
+        elif method == 'INTER_CUBIC':
+            interp_method = cv2.INTER_CUBIC
+        elif method == 'INTER_LANCZOS4':
+            interp_method = cv2.INTER_LANCZOS4
+        elif method == 'INTER_AREA':
+            interp_method = cv2.INTER_AREA
+        else:
+            raise ValueError(f"Unknown interpolation method: {method}")
+        
+        img_resized = cv2.resize(img, (target_res, target_res), interpolation=interp_method)
+        return img_resized
+
+    def imshow(self):
+        plt.imshow(self.img)
+        plt.show()
+    
+    def find_body_bbox(self):
+        """
+        Detects the bounding box of the largest contour (assumed to be the body).
+        """
+        img = self.img
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) if len(img.shape) == 3 else img
+        _, thresh = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if not contours:
+            return None
+        largest = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest)
+        return np.array([x, y, w, h])
+
+    @staticmethod
+    def determine_max_BB(source_BB,target_BB):
+        x, y, w, h = source_BB
+        # Find the smallest target_bb that fits max(w, h)
+        max_size = max(w,h)
+        # Filter values that are >= size_needed
+        valid_sizes = target_BB[target_BB >= max_size]
+        if len(valid_sizes) == 0:
+            raise ValueError("No bounding box size can fit the object!")
+        # Take the minimum among valid sizes
+        selected_size = np.min(valid_sizes) # Save this one
+        print(f"Selected bounding box size: {selected_size}")
+        return float(selected_size)
+
+    @staticmethod
+    def BB_random_padding(source_BB,selected_target_size, img_res, max_selected_target_size):
+        # Extract BB coordinates
+        x, y, w, h = source_BB
+        # Initialize error index to 0
+        error_index = 0
+        # Compute the horizontal and vertical padding needed to go from a rectangular source_BB to a square target_BB
+        delta_all = selected_target_size - np.array([w, h])  # [Δx, Δy]
+        # Determine maximum left-right paddings in the horizontal direction
+        check_1 = check_2 = 0
+        if delta_all[0] >= 0:
+            max_delta_1 = min(delta_all[0], x)  # left
+            max_delta_2 = min(delta_all[0], img_res[0] - x - w)  # right
+            check_1 = 1
+        else:
+            error_index = 1
+        # Determine maximum top-bottomw paddings in the vertical direction
+        if delta_all[1] >= 0:
+            max_delta_3 = min(delta_all[1], y)  # top
+            max_delta_4 = min(delta_all[1], img_res[1] - y - h)  # bottom
+            check_2 = 1
+        else:
+            error_index = 2
+        # Initialize the values of the four random padding values
+        delta_1 = delta_2 = delta_3 = delta_4 = 0
+        check = True
+        # Determine coherent values for all delta's
+        w_new = selected_target_size
+        h_new = selected_target_size
+        delta_1234 = np.zeros((1,4))
+        if selected_target_size == max_selected_target_size:
+            x_new, y_new = 1, 1
+        else:
+            while check:
+                if check_1:
+                    delta_1 = np.random.randint(0, max(max_delta_1, 1))
+                    delta_2 = delta_all[0] - delta_1
+                if check_2:
+                    delta_3 = np.random.randint(0, max(max_delta_3, 1))
+                    delta_4 = delta_all[1] - delta_3
+                if (delta_1 <= max_delta_1 and delta_2 <= max_delta_2 and
+                    delta_3 <= max_delta_3 and delta_4 <= max_delta_4):
+                    check = False
+            # Pack all delta's (left, right, top, bottom) of the final bounding box            
+            delta_1234 = np.array([int(delta_1), int(delta_2), int(delta_3), int(delta_4)])
+            # Generate the new bounding box
+            x_new = source_BB[0] - delta_1234[0]
+            y_new = source_BB[1] - delta_1234[2]
+        new_BB = np.array([int(x_new), int(y_new), int(w_new), int(h_new)])
+        # Activate warnings for I's
+        if error_index == 1: 
+            warnings.warn(f"Width of bounding box ({w}) exceeds selected target size ({selected_target_size}).")
+        elif error_index == 2:
+            warnings.warn(f"Width of bounding box ({h}) exceeds selected target size ({selected_target_size}).")
+        
+        return new_BB, delta_1234, error_index
+
+    def ProceduralRandomPadding(self, target_BB_sizes, original_img_size, final_img_size):
+
+        ### ProceduralRandomPadding and resizing of the image
+        # Determine the Bounding Box (BB) in the body in the image
+        original_BB = self.find_body_bbox() # Call this img_BB
+        # Determine the target BB size
+        target_BB_size = self.determine_max_BB(original_BB,target_BB_sizes)
+        # Perform random padding
+        final_BB,delta_1234, error_index = self.BB_random_padding(original_BB, target_BB_size, (original_img_size,original_img_size), original_img_size)
+        # Crop the image with BB
+        img_cropped = DataProcessing.crop_image_by_BB(self.img,final_BB)
+        # Reduce the image to final resolution 
+        img_resized = self.resize_image(img_cropped,final_img_size,'INTER_AREA')
+
+        ### ProceduralRandomPadding and resizing of the label        
+        if "CoM" in self.label:
+            CoM_u_S0, CoM_v_S0 = self.label["CoM"]
+            # Compute CoM in S1 
+            CoM_u_S1 = CoM_u_S0 - final_BB[0]
+            CoM_v_S1 = CoM_v_S0 - final_BB[1]
+            # Compute CoM in S2
+            CoM_u_S2 = CoM_u_S1 * (final_img_size / target_BB_size)
+            CoM_v_S2 = CoM_v_S1 * (final_img_size / target_BB_size)
+
+            label_resized = {
+                'CoM': (CoM_u_S0,CoM_v_S0), 
+                'CoM_S1': (CoM_u_S1,CoM_v_S1), 
+                'CoM_S2': (CoM_u_S2,CoM_v_S2), 
+            }
+        return img_resized, img_cropped, original_BB, final_BB, delta_1234, label_resized
--- scripts/PostProcess_Eq_DB.py
+++ scripts/PostProcess_Eq_DB.py
@@ -1,133 +1,71 @@
 import os, sys
 import numpy as np
-import matplotlib.patches as patche
+import matplotlib.patches as patches
 from matplotlib import pyplot as plt
 
 import warnings
 from glob import glob
 import cv2
 
-
 sys.path.append(os.getcwd())
 import cortopy as corto
 
+# TODO: add label change for the CoM after ProceduralRandomPadding
 
-#TODO: Move this functions into the PostProcessing class
-def find_body_bbox(image):
-    """
-    Detects the bounding box of the largest contour (assumed to be the body).
-    """
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) if len(image.shape) == 3 else image
-    _, thresh = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if not contours:
-        return None
-    largest = max(contours, key=cv2.contourArea)
-    x, y, w, h = cv2.boundingRect(largest)
-    return x, y, w, h
-
-def determine_max_BB(source_BB,target_BB):
-    x, y, w, h = source_BB
-    # Find the smallest target_bb that fits max(w, h)
-    max_size = max(w,h)
-    # Filter values that are >= size_needed
-    valid_sizes = target_BB[target_BB >= max_size]
-    if len(valid_sizes) == 0:
-        raise ValueError("No bounding box size can fit the object!")
-    # Take the minimum among valid sizes
-    selected_size = np.min(valid_sizes) # Save this one
-    print(f"Selected bounding box size: {selected_size}")
-    return float(selected_size)
-
-def BB_random_padding(source_BB,selected_target_size, img_res, max_selected_target_size):
-    # Extract BB coordinates
-    x, y, w, h = source_BB
-    # Initialize error index to 0
-    error_index = 0
-    # Compute the horizontal and vertical padding needed to go from a rectangular source_BB to a square target_BB
-    delta_all = selected_target_size - np.array([w, h])  # [Δx, Δy]
-    # Determine maximum left-right paddings in the horizontal direction
-    check_1 = check_2 = 0
-    if delta_all[0] >= 0:
-        max_delta_1 = min(delta_all[0], x)  # left
-        max_delta_2 = min(delta_all[0], img_res[0] - x - w)  # right
-        check_1 = 1
-    else:
-        error_index = 1
-    # Determine maximum top-bottomw paddings in the vertical direction
-    if delta_all[1] >= 0:
-        max_delta_3 = min(delta_all[1], y)  # top
-        max_delta_4 = min(delta_all[1], img_res[1] - y - h)  # bottom
-        check_2 = 1
-    else:
-        error_index = 2
-    # Initialize the values of the four random padding values
-    delta_1 = delta_2 = delta_3 = delta_4 = 0
-    check = True
-    # Determine coherent values for all delta's
-    w_new = selected_target_size
-    h_new = selected_target_size
-    if selected_target_size == max_selected_target_size:
-        x_new, y_new = 1, 1
-    else:
-        while check:
-            if check_1:
-                delta_1 = np.random.randint(0, max(max_delta_1, 1))
-                delta_2 = delta_all[0] - delta_1
-            if check_2:
-                delta_3 = np.random.randint(0, max(max_delta_3, 1))
-                delta_4 = delta_all[1] - delta_3
-            if (delta_1 <= max_delta_1 and delta_2 <= max_delta_2 and
-                delta_3 <= max_delta_3 and delta_4 <= max_delta_4):
-                check = False
-        # Pack all delta's (left, right, top, bottom) of the final bounding box            
-        delta_1234 = np.array([int(delta_1), int(delta_2), int(delta_3), int(delta_4)])
-        # Generate the new bounding box
-        x_new = source_BB[0] - delta_1234[0]
-        y_new = source_BB[1] - delta_1234[2]
-    new_BB = np.array([int(x_new), int(y_new), int(w_new), int(h_new)])
-    # Activate warnings for I's
-    if error_index == 1: 
-        warnings.warn(f"Width of bounding box ({w}) exceeds selected target size ({selected_target_size}).")
-    elif error_index == 2:
-        warnings.warn(f"Width of bounding box ({h}) exceeds selected target size ({selected_target_size}).")
-    return new_BB, delta_1234, error_index
-
-# Crop function
-def crop_image_by_BB(img, BB):
-    x, y, w, h = BB
-    return img[y:y+h, x:x+w]
-
-# Main script
+### Set image path ###
 input_folder = "output/SXX_Double/img"  # Folder with input images
 image_paths = glob(os.path.join(input_folder, "*.png"))  # or .jpg, etc.
 file_names = [os.path.basename(path) for path in image_paths]
 
-target_BB = np.array([2048, 1024, 512, 256, 128])
+### (PART-1) Add artificial noise ###
 
-for ii in range(0,21):
-ii = 10
-image = cv2.imread(input_folder + '/' + file_names[ii])
-print(ii)
-plt.imshow(image)
-plt.show()
+# output_path_noise = "output/SXX_Double/img_noise"
+# noise_settings = ...
+# corto.DataProcessing.add_artificial(input_path, output_path_noise, noise_settings)
 
-source_BB = find_body_bbox(image) # Save these 4
-# Determine the selected target size
-selected_target_size = determine_max_BB(source_BB,target_BB)
-# Perform random padding
-a,b,c = BB_random_padding(source_BB, selected_target_size, (2048,2048), 2048)
-# Crop the image with BB
-img_cropped = crop_image_by_BB(image, a)
-# Reduce image size to 128x128
-img_resized = cv2.resize(img_cropped, (128, 128), interpolation=cv2.INTER_AREA)
-# Plot image with bounding boxes
+### (PART-2) Adaptive resizing ###
+
+# Define target bounding boxes for the cropped-version of the images
+target_BB_sizes = np.array([2048, 1024, 512, 256, 128])
+target_img_size = 128
+img_size = (2048,2048)
+label = {
+    'CoM': (1024,1024)
+}
+
+ii = 20 # Just analyze one image
+# CRASH ON 5 and 20 (10% of the cases, multiple blobs)
+
+# Initialize a DataProcessing object (defined by an img-label pair)
+
+img_path = input_folder + '/' + file_names[ii]
+data = corto.DataProcessing(img_path = img_path, label = label )
+
+# data.imshow()
+
+original_img_size = 2048
+final_img_size = 128
+
+img_resized, img_cropped, original_BB, final_BB, delta_1234,label_resized  = data.ProceduralRandomPadding(target_BB_sizes, original_img_size, final_img_size)
+
+CoM_S0 = label_resized["CoM"]
+CoM_S2 = label_resized["CoM_S2"]
+
+# Plots
+
 fig, ax = plt.subplots(1)
-ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+ax.imshow(cv2.cvtColor(data.img, cv2.COLOR_BGR2RGB))
+plt.plot(CoM_S0[0],CoM_S0[1],'rx')
 # Draw bounding boxes
-for bb, color in zip([source_BB, a], ['red', 'blue']):
+for bb, color in zip([original_BB, final_BB], ['red', 'blue']):
     rect = patches.Rectangle((bb[0], bb[1]), bb[2], bb[3], linewidth=2, edgecolor=color, facecolor='none')
     ax.add_patch(rect)
 plt.title("Image with Two Bounding Boxes")
 plt.axis('off')
+
+
+plt.figure()
+plt.imshow(img_resized)
+plt.plot(CoM_S2[0],CoM_S2[1],'rx')
+
 plt.show()
